daca4922a156900c7ab2b4bb60638d1c2c703a88
David Fridovich-Keil <dfk@berkeley.edu>
2018-10-02 10:16:04 -0700

    cleanup. docs moved to gh-pages

--- documentation/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['dualscalar',['DualScalar',['../structautodifk_1_1forward_1_1_dual_scalar.html',1,'autodifk::forward']]]
-];
--- documentation/html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['forwardautodiff',['ForwardAutodiff',['../classautodifk_1_1forward_1_1_forward_autodiff.html',1,'autodifk::forward']]]
-];
